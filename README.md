d25e6d53f9ad62ff0a12095abab2e0673c0cdf7f
hacker1db <hacker1db@outlook.com>
2016-11-20 20:55:39 -0800

    adds tool repository, installs git and PTF framework

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 #active_defense cookbook 
 
  This cookbook will install security tools, ADHD, Bro, pentester framework and much more. 
-
+ - https://github.com/trustedsec/ptf.git
+ - 
  - This is will be a work in progress. 
 
  Thanks,
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+directory '/tools' do
+  action :create
+end
+package 'git' do 
+  action :install
+end
+git 'https://github.com/trustedsec/ptf.git' do
+  destination '/tools'
+  revision 'master'
+  repository 'https://github.com/trustedsec/ptf.git'
+  action :checkout
+end
