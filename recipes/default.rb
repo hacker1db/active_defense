d0614e8dae1d7a42813167ff516a75acdc4af1ec
hacker1db <hacker1db@outlook.com>
2016-11-24 11:56:56 -0800

    dir for all tools under /tools/nameoftool still working on adhd issue #3

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,8 +10,11 @@ end
 package 'git' do
   action :install
 end
+directory '/home/vagrant/tools/ptf' do 
+  action :create 
+end
 git 'https://github.com/trustedsec/ptf.git' do
-  destination '/home/vagrant/tools'
+  destination '/home/vagrant/tools/ptf'
   revision 'master'
   repository 'https://github.com/trustedsec/ptf.git'
   action :checkout
@@ -24,4 +27,15 @@ git 'git://git.bro.org/bro' do
   revision 'master'
   repository 'git://git.bro.org/bro'
   action :checkout
+end
+directory '/home/vagrant/tools/adhd' do 
+  action :create 
+end 
+script 'install_ADHD' do
+  interpreter 'bash'
+  user 'vagrant'
+  cwd '/tools/adhd'
+  code <<-EOH
+  bash -c "$(curl -sL https://raw.githubusercontent.com/adhdproject/buildkit/master/adhd-install.sh)
+  EOH
 end
